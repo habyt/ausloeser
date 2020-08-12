02041c2a9526ddd9d9b8ed64c16893b5f6ceeeb3
Mischa Holz <me@mhlz.de>
2020-08-12 10:52:55 +0200

    Load the issue

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-    const payload = JSON.stringify(github.context.payload, null, 4);
-    console.log(payload);
-} catch (e) {
-    core.setFailed(e.message);
+async function main() {
+    try {
+        if (github.context.payload.pull_request === undefined) {
+            return;
+        }
+
+        const payload = github.context.payload;
+
+        const token = core.getInput("pat");
+        const octokit = github.getOctokit(token);
+
+        const issue = await octokit.issues.get({
+            owner: payload.repository.owner,
+            repo: payload.repository.name,
+            issue_number: payload.issue.number,
+        });
+
+        console.log(JSON.stringify(issue, null, 4));
+
+        const payload = JSON.stringify(github.context.payload, null, 4);
+        console.log(payload);
+    } catch (e) {
+        core.setFailed(e.message);
+    }
 }
+
+main();
